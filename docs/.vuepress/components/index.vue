922c02c401a9f7257321b9747772acd14cb1e00c
bong-u <bongudev@gmail.com>
2023-01-12 17:47:49 +0900

    ✨ Get git logs

---
+++ docs/.vuepress/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>hello</h1>
+</template>
+
+<script>
+    // import { getLog } from './GitModule.js';
+    // const getLog = require('./GitModule');
+    import getLog from './GitModule.js';
+    export default {
+        created() {
+            console.log(getLog());
+        },
+        methods : {
+        }
+    }
+        
+</script>
