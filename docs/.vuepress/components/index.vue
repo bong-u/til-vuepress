334e3112ff070b31c24e3069eba44fef0ef0017d
bong-u <bongudev@gmail.com>
2023-01-26 21:54:12 +0900

    🔥 remove calendar

--- docs/.vuepress/components/index.vue
+++ docs/.vuepress/components/index.vue
@@ -1,37 +1,13 @@
 <template>
-  <article>
-    <h4>Studied for {{ countOfDays }} days in the last year</h4>
-    <div id="calendar_container"></div>
-  </article>
+  <article></article>
 </template>
 
 <script>
-import git_calendar from "./GitCalendar.js";
-export default {
-  data: () => ({
-    countOfDays: git_calendar.getCountOfDays(),
-  }),
-  created() {
-    git_calendar.init();
-  },
-  mounted() {
-    const container = document.getElementById("calendar_container");
-    container.appendChild(git_calendar.createSVG(document));
-  },
-};
+export default {};
 </script>
 
 <style>
 article {
   max-width: 810px;
-}
-#calendar_container {
-  padding: 10px 15px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  overflow: hidden;
-  border: 1px solid #30363d;
-  border-radius: 6px;
 }
 </style>
