3feaec085450106e2c984450be0d1dd6c8bb7fb6
bong-u <bongudev@gmail.com>
2023-01-26 21:53:59 +0900

    🔥 remove unnecessary comment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,58 +1,46 @@
-const { description } = require('../../package');
-const { readdirSync, statSync } = require('fs');
+const { description } = require("../../package");
+const { readdirSync, statSync } = require("fs");
 
-const baseDir = 'docs/';
+const baseDir = "docs/";
 const dirs = readdirSync(baseDir).filter((file) => {
-  return file !== ".vuepress" && statSync(baseDir+file).isDirectory();
+  return file !== ".vuepress" && statSync(baseDir + file).isDirectory();
 });
 
-const getFile = dir => {
+const getFile = (dir) => {
   const path = `docs/${dir}`;
   const files = readdirSync(path);
   const fileList = [];
   files.map((file) => {
-    fileList.push(dir+'/'+file);
+    fileList.push(dir + "/" + file);
   });
   return fileList;
 };
 
 const sidebarItems = [];
-dirs.forEach(dir => {
+dirs.forEach((dir) => {
   var obj = {
     title: dir,
-    children: getFile(dir)
+    children: getFile(dir),
   };
   sidebarItems.push(obj);
 });
 
 module.exports = {
-  //  Ref：https://v1.vuepress.vuejs.org/config/#title
   title: "BONG-U's TIL",
   base: "/til/",
-  
-  // Ref：https://v1.vuepress.vuejs.org/config/#description
-  description: description,
-  
-  // Ref：https://v1.vuepress.vuejs.org/config/#head
-  head: [
-    ["link", { rel: "icon", href: "avata.png" }],
-  ],
 
-  // Ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+  description: description,
+
+  head: [["link", { rel: "icon", href: "avata.png" }]],
+
   themeConfig: {
-    repo: '',
+    repo: "",
     editLinks: false,
-    docsDir: '',
-    editLinkText: '',
+    docsDir: "",
+    editLinkText: "",
     lastUpdated: false,
     sidebar: sidebarItems,
   },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
